58fc58113febccd1769cb413b9dbb42cfe745dc2
milhoranca21-del <milhoranca21@gmail.com>
2026-03-15 18:51:57 -0300

    acabou

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
     <body>
         
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
+        <div class="container-fluid">
         <a class="navbar-brand" href="/">Sistema</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -218,19 +218,19 @@ app.post('/cadastrofornecedor',(req,res) => {
 
     <body>
 
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/">Sistema</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-                <li class="nav-item">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            a class="navbar-brand" href="/">Sistema</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
                     <a class="nav-link" href="/">Home</a>
-                </li>
+                    </li>
                 
-                <li class="nav-item dropdown">
+                    <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="fornecedorDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Fornecedor
                     </a>
@@ -238,15 +238,15 @@ app.post('/cadastrofornecedor',(req,res) => {
                         <li><a class="dropdown-item" href="/cadastrofornecedor">Cadastrar Fornecedor</a></li>
                         <li><a class="dropdown-item" href="/listacadastro">Lista de Fornecedores</a></li>
                     </ul>
-                </li>
-                <li class="nav-item">
+                    </li>
+                    <li class="nav-item">
                     <a class="nav-link" href="/login">Login</a>
-                </li>
+                    </li>
                 
-            </ul>
+                    </ul>
+            </div>
         </div>
-    </div>
-</nav>
+        </nav>
 
         <div class="container mt-5">
 
